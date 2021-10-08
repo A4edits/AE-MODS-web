63c78e65112c55775754b632d8678ab8976aa67b
A4edits <90356972+A4edits@users.noreply.github.com>
2021-10-08 18:59:36 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,15 +11,15 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "http://i.imgur.com/NKmDThu.jpeg";
+  r_text[0] = "http://imgur.com/gallery/NKmDThu.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919967413665*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater A4edits*
+*owner number wa.me/919967413665?text=Hi%20A4%20edits.%20*
 
 *Bot making video : soon available*
 
